8a2c685cc93a117112921a5b59900dcfc380dfe0
jvera71 <j_vera@hotmail.com>
2026-06-10 00:18:57 +0200

    Agregar archivos de proyecto.

---
+++ src/NotasVozInteligentes/Program.cs
@@ -0,0 +1,38 @@
+using NotasVozInteligentes.Components;
+
+namespace NotasVozInteligentes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveWebAssemblyComponents();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+            app.Run();
+        }
+    }
+}
